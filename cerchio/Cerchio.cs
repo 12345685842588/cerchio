0ab88895ee10a71714da0d5d4e5c119bc44ebf43
Utente <Utente@DESKTOP-BAB4AAS>
2024-01-24 15:50:39 +0100

    Aggiungere i file di progetto.

---
+++ cerchio/Cerchio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio
+{
+    internal class Cerchio
+    {
+        public float Raggio { get; set; }
+
+        public Cerchio( float raggio)
+        {
+            this.Raggio = raggio;
+        }
+
+        public float Area()
+        {
+            return (float)(Math.PI* Raggio*Raggio);
+        }
+
+        public float Perimetro()
+        {
+            return (float)(2 * Raggio * Math.PI);
+        }
+    }
+}
---
+++ cerchio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserire raggio");
+            float r=float.Parse(Console.ReadLine());
+            Cerchio c = new Cerchio(r);
+
+            Console.WriteLine("area={0} e perimetro={1}", c.Area(), c.Perimetro());
+
+            Console.ReadLine();
+        }
+    }
+}
